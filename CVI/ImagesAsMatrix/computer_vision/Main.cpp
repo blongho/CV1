e5441cd0282bd01f57abaf77c5e8ec3dd9180df0
Bernard Che Longho <lobe1602@student.miun.se>
2020-05-15 13:47:13 +0200

    Initial setup with a hello world

---
+++ CVI/ImagesAsMatrix/computer_vision/Main.cpp
@@ -0,0 +1,7 @@
+#include "Base.h"
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
---
+++ CVI/ImagesAsMatrix/computer_vision/headers/Base.h
@@ -0,0 +1,10 @@
+﻿//Include file for standard system include files,
+// or project specific include files.
+
+
+#pragma once
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+// TODO: Reference additional headers your program requires here.
