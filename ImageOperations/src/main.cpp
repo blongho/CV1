3cd5bbae3c51f902bc379ed8c87df307470d2cc6
Bernard Che Longho <lobe1602@student.miun.se>
2020-05-26 18:02:55 +0200

    Cropping a region of an image

--- ImageOperations/src/main.cpp
+++ ImageOperations/src/main.cpp
@@ -5,6 +5,8 @@ int main()
  1. Creating an image
   We use Mat(height, width, datatypeOfTheMatrix,
 */
+    try
+    {
     const auto height = 200; 
     const auto width = 400;
     Mat emptyMatrix = Mat(height, width,CV_8UC3, Scalar(0, 0, 0));
@@ -24,7 +26,35 @@ int main()
     Mat emptyFromAnother = Mat(emptyMatrix.size(), emptyMatrix.type(), Scalar(100, 100, 100));
 
     imshow("Copied from original", emptyFromAnother);
+
+
+    // Cropping an image
+    // We need to know where to crop i.e. the range of the rows and columns of interest
+    
+    const std::string path = "D:\\images\\Grab\\image (186).png";
+   
+        Mat hand = imread(path, -1);
+        cout << "Size of hand " << hand.size() << endl;
+        imshow("See me", hand);
+
+        // Crop using some random values
+        // x coordinates = 170 to 320
+        // y coordinates = 40 to 200
+        Mat croppedHand = hand(Range(40, 200), Range(170, 320));
+        imshow("Cropped hand", croppedHand);
+    }
+    catch (const std::exception&e)
+    {
+        cerr << "Error " << e.what() << endl;
+    }
+    catch (const cv::Exception & cve) {
+        cerr << "Error " << cve.what() << endl;
+    }
+   
+
+
     const auto key = waitKey(0);
-    cout << "Key pressed is : ascii value = " << key << ", printable: "  << static_cast<char>(key) << endl;
+    cout << "Key pressed is : ascii value = " << key << ", printable: " << static_cast<char>(key) << endl;
+
     return 0;
 }
