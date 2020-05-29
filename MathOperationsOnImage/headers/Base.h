184bbe9d11e7ebdcaa8d5e0a7a571a2824555b8d
Bernard Che Longho <lobe1602@student.miun.se>
2020-05-29 09:58:58 +0200

    Changing image brightnes

--- MathOperationsOnImage/headers/Base.h
+++ MathOperationsOnImage/headers/Base.h
@@ -9,10 +9,15 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace cv;
 using std::cout;
 using std::endl;
 using std::cerr; 
+
+#define type2str(int) cv::typeToString(int)
 void imageInfo(const Mat& image);
+
+void imageInfo(const char* info, const Mat& image);
 #endif
 // TODO: Reference additional headers your program requires here.
--- MathOperationsOnImage/src/main.cpp
+++ MathOperationsOnImage/src/main.cpp
@@ -9,17 +9,18 @@ int main()
 		const double scalingFactor = 1 / 255.0;
 		const double shift = 0;
 		Mat contrastHigh = image.clone();
+		Mat bright = image.clone();
 
-		imshow("Origingal image ", image);
-		imageInfo(image);
+		//imshow("Origingal image ", image);
+		//imageInfo(image);
 		//Converting from unsigned char to float(32bit)
 		image.convertTo(image, CV_32FC3, scalingFactor, shift);
-		imshow("Converted image", image);
-		imageInfo(image);
+		//imshow("Converted image", image);
+		//imageInfo(image);
 		//Converting from float to unsigned char
 		image.convertTo(image, CV_8UC3, 1.0 / scalingFactor, shift);
-		imageInfo(image);
-		imshow("Restored image", image);
+		//imageInfo(image);
+		//imshow("Restored image", image);
 
 
 		// Changing the contrast
@@ -29,9 +30,38 @@ int main()
 		contrastHigh.convertTo(contrastHigh, CV_64F);
 		contrastHigh = contrastHigh * (1 + contrastPercentage / 100.0);
 
-		imshow("Improved contrast ", contrastHigh);
-		imageInfo(contrastHigh);
+		//imshow("Improved contrast ", contrastHigh);
+		//imageInfo(contrastHigh);
+
+		// Altering the brightness simply implies adding/subtracting some offset value to/from the RGB values
+		const int brightnessOffset = 30;
+		Mat brightHigh = image.clone();
+		Mat brightHighChannels[3];
+		split(brightHigh, brightHighChannels);
+
+		for (int i = 0; i < 3; i++) {
+			add(brightHighChannels[i], brightnessOffset, brightHighChannels[i]);
+		}
+
+		merge(brightHighChannels, 3, brightHigh);
+
+		imshow("Original Image", image);
+		imshow("Brightened Image", brightHigh);
+		imshow("Constract Image", contrastHigh);
+		double minVal, maxVal;
+		cout << "Original Image Datatype : " << type2str(image.type()) << endl;
+		cout << "Contrast Image Datatype : " << type2str(contrastHigh.type()) << endl;
+		cout << "Brightness Image Datatype : " << type2str(brightHigh.type()) << endl;
+
+		minMaxLoc(image, &minVal, &maxVal);
+		cout << "Original Image Highest Pixel Intensity : " << maxVal << endl;
+		minMaxLoc(contrastHigh, &minVal, &maxVal);
+		cout << "Contrast Image Highest Pixel Intensity : " << maxVal << endl;
+		minMaxLoc(brightHigh, &minVal, &maxVal);
+		cout << "Brightness Image Highest Pixel Intensity : " << maxVal << endl;
+
 	}
+
 	catch (const std::exception & e)
 	{
 		cerr << "Error " << e.what() << endl;
