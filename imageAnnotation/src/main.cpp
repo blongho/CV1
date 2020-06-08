20a6e782a068c43ac7d8fb4b9ad369e5b5752f68
Bernard Che Longho <lobe1602@student.miun.se>
2020-06-08 17:11:32 +0200

    Drawing a circle on an image

--- imageAnnotation/src/main.cpp
+++ imageAnnotation/src/main.cpp
@@ -16,6 +16,22 @@ int main(int argc, char** argv)
 		
 		imshow("Boy with score", imageLine);
 	
+		// Drawing a circle in an image
+		
+		Mat imageCircle;
+		image.copyTo(imageCircle);
+
+		circle(imageCircle, Point{ 250, 125 }, 100, Scalar{ 255, 0, 0 }, 5, LINE_AA);
+
+		imshow("Boy with circle", imageCircle);
+
+		Mat filledHead;
+		image.copyTo(filledHead);
+
+		// Specifying the thickness to -1 fills the circle
+		circle(filledHead, Point{ 250, 125 }, 100, Scalar{ 0, 0, 255 }, -1, LINE_AA);
+
+		imshow("Boy with filled circle", filledHead);
 	}
 
 	catch (const std::exception & e)
