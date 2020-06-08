c16aa580fb6202763acf54a0f282d4cc61b68198
Bernard Che Longho <lobe1602@student.miun.se>
2020-06-08 17:35:54 +0200

    Drawing an ellipse on an image

--- imageAnnotation/src/main.cpp
+++ imageAnnotation/src/main.cpp
@@ -32,6 +32,17 @@ int main(int argc, char** argv)
 		circle(filledHead, Point{ 250, 125 }, 100, Scalar{ 0, 0, 255 }, -1, LINE_AA);
 
 		imshow("Boy with filled circle", filledHead);
+
+		// Drawing an ellipse
+		
+		Mat imageEllipse;
+		image.copyTo(imageEllipse);
+
+		ellipse(imageEllipse, Point(250, 125), Point(100, 50), 0, 0, 360, Scalar(255, 0, 0), 3, LINE_AA);
+
+		ellipse(imageEllipse, Point(250, 125), Point(100, 50), 90, 0, 360, Scalar(0, 0, 255), 3, LINE_AA);
+
+		imshow("Image with ellipse", imageEllipse);
 	}
 
 	catch (const std::exception & e)
@@ -39,7 +50,7 @@ int main(int argc, char** argv)
 		cerr << "Error " << e.what() << endl;
 	}
 	
-
+	
 
 
 	const auto key = waitKey(0);
